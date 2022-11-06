789fd02d57703204dda5fc6560799f5280aa0fe2
Shariq Naiyer <shariqnaiyer@gmail.com>
2022-11-06 10:57:37 -0700

    final

--- getimages.py
+++ getimages.py
@@ -2,8 +2,11 @@ from selenium import webdriver
 import time
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from PIL import Image
+import os
+
 chrome_options = Options() 
-chrome_options.headless = True
+# chrome_options.add_experimental_option("detach", True)
 prefs = {"download.default_directory" : r"C:\Users\shariq\Desktop\Projects\readify\images\\","directory_upgrade": True}
 chrome_options.add_experimental_option("prefs",prefs)
 
@@ -17,6 +20,8 @@ for k in text:
     driver = webdriver.Chrome()
     driver = webdriver.Chrome(options=chrome_options)
     driver.get("http://www.wombo.art/create")
+
+    time.sleep(5)
 
     inputElement = driver.find_element(By.XPATH, """//*[@id="blur-overlay"]/div/div/div[1]/div[1]/div[1]/div[1]/div[1]/input""")
     inputElement.send_keys(k)
@@ -37,3 +42,14 @@ for k in text:
 
 driver.close()
 
+dir_path = os.path.dirname(os.path.realpath(__file__))+ "/images/"
+res = []
+for path in os.listdir(dir_path):
+    if os.path.isfile(os.path.join(dir_path, path)):
+        res.append(path)
+print(res)
+
+for i in res:
+    im = Image.open('images/'+i)
+    cropped = im.crop((60,208,1020,1778))
+    cropped.save('images/'+i)
